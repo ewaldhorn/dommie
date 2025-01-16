9bdaf04847a15f873e35a2dda2fe7475ac31fc62
Ewald Horn <ewaldhorn@gmail.com>
2025-01-16 17:20:03 +0200

    Add more dev docs

--- dom/dom_event_listeners.go
+++ dom/dom_event_listeners.go
@@ -3,6 +3,9 @@ package dom
 import "syscall/js"
 
 // ----------------------------------------------------------------------------
+//
+// Wraps a Go function and sets it as the callback for the specified event on
+// the target element
 func AddEventListener(elem string, event string, fn func()) {
 	GetElementById(elem).Call("addEventListener", event, js.FuncOf(wrapGoFunction(fn)))
 }
--- dom/dom_go_wrapper.go
+++ dom/dom_go_wrapper.go
@@ -3,6 +3,9 @@ package dom
 import "syscall/js"
 
 // ----------------------------------------------------------------------------
+//
+// Wraps a Go function inside a JavaScript function. Handy for when you need to
+// run Go code in event listeners etc.
 func wrapGoFunction(fn func()) func(js.Value, []js.Value) interface{} {
 	return func(_ js.Value, _ []js.Value) interface{} {
 		fn()
